a46975d3146c71f2bb629ee48abcd0b4afc7db9e
Umananda Siddha <umanandasiddha243@gmail.com>
2024-02-26 23:36:42 +0530

    test identifier added

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -38,7 +38,7 @@ const Home = ({ user }: PropsType) => {
                                 Get started
                             </button>
                             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                                Testing 2 <span aria-hidden="true">→</span>
+                                Testing 3 <span aria-hidden="true">→</span>
                             </a>
                         </div>
                     </div>
