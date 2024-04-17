fad47eb7619e3a57e844ae72a0e773a08d476d1e
RITAP DEY <ritapdey@RITAPs-MacBook-Air.local>
2024-04-17 19:29:48 +0530

    tree card updated

--- client/src/pages/cards/inputs/tree.tsx
+++ client/src/pages/cards/inputs/tree.tsx
@@ -1,13 +1,13 @@
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import axios from "axios";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 import { inputs } from "@/types/form-inputs";
@@ -21,122 +21,147 @@ import { treeNotTemp } from "@/redux/reducer/treeReducer";
 import { Tree } from "@/types/types";
 
 const CreateTree = () => {
+  const navigate = useNavigate();
+  const [search] = useSearchParams();
+  const id = search.get("treeId");
+  const dispatch = useDispatch();
 
-    const navigate = useNavigate();
-    const [search] = useSearchParams();
-    const id = search.get("treeId");
-    const dispatch = useDispatch();
+  const [card, setCard] = useState<Tree | null>();
+  const [isTree, setIsTree] = useState<boolean>(id ? true : false);
+  const [treeLoading, setTreeLoading] = useState<boolean>(false);
 
-    const [card, setCard] = useState<Tree | null>();
-    const [isTree, setIsTree] = useState<boolean>(id ? true : false);
-    const [treeLaoding, setTreeLoading] = useState<boolean>(false);
+  const { user, isPaid } = useSelector((state: RootState) => state.userReducer);
 
-    const { user, isPaid } = useSelector(
-        (state: RootState) => state.userReducer
-    );
-    
-    useEffect(() => {
-        const fetchTree = async () => {
-            if (id) {
-                try {
-                    const { data }: { data: SingleTreeResponse } = await axios.get(`${import.meta.env.VITE_BASE_URL}/cards/detailed/${id!}?type=tree`, { withCredentials: true });
-                    setIsTree(true);
-                    setCard(data.vCard);
-                } catch (error: any) {
-                    toast.error(error.response.data.message);
-                    dispatch(treeNotTemp());
-                }
-            }
-        }
-        const cardData = localStorage.getItem("current_card");
-        if (cardData) {
-            setCard(JSON.parse(cardData));
-            if (card?._id !== id) {
-                fetchTree();
-            }
-        } else {
-            fetchTree();
-        }
-    }, []);
-
-    const form = useForm({
-        defaultValues: {
-            name: card ? card.name : "",
-            scientificName: card?.scientificName || "",
-            treeType: card?.treeType || "",
-            location: card?.location || "",
-            description: card?.description || "",
-            features: card?.features || "",
-            maintenance: card?.maintenance || "",
-            benefits: card?.benefits || "",
-            funFact: card?.funFact || "",
-        },
-    });
-
-    const onSubmit = async (values: any) => {
-        setTreeLoading(true);
-        const treeData = {
-            name: values.name,
-            scientificName: values.scientificName,
-            treeType: values.treeType,
-            location: values.location,
-            description: values.description,
-            features: values.features,
-            maintenance: values.maintenance,
-            benefits: values.benefits,
-            funFact: values.funFact,
-            user: user?._id
-        }
+  useEffect(() => {
+    const fetchTree = async () => {
+      if (id) {
         try {
-            if (isTree) {
-                await axios.put(`${import.meta.env.VITE_BASE_URL}/cards/edit/${id}?type=tree`, treeData, { withCredentials: true });
-                toast.success("Tree VCard Updated");
-            } else {
-                await axios.post(`${import.meta.env.VITE_BASE_URL}/cards/new?type=tree`, treeData, { withCredentials: true });
-                toast.success("Tree VCard Created");
-            }
-            if (isPaid || user?.role === "admin") {
-                navigate(-1);
-            } else {
-                navigate("/plans");
-            }
+          const { data }: { data: SingleTreeResponse } = await axios.get(
+            `${import.meta.env.VITE_BASE_URL}/cards/detailed/${id!}?type=tree`,
+            { withCredentials: true }
+          );
+          setIsTree(true);
+          setCard(data.vCard);
         } catch (error: any) {
-            toast.error(error.response.data.message);
-            if (!isPaid && user?.role !== "admin") {
-                navigate("/plans");
-            }
+          toast.error(error.response.data.message);
+          dispatch(treeNotTemp());
         }
-        setTreeLoading(false);
+      }
+    };
+    const cardData = localStorage.getItem("current_card");
+    if (cardData) {
+      setCard(JSON.parse(cardData));
+      if (card?._id !== id) {
+        fetchTree();
+      }
+    } else {
+      fetchTree();
     }
+  }, []);
 
-    return (
-        <div className="flex flex-col justify-center items-center min-h-screen mb-8">
-            <p>{card?.name!}</p>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    {
-                        inputs.map((input) => (
-                            <FormField
-                                key={input.name}
-                                control={form.control}
-                                name={input.name}
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>{input.label}</FormLabel>
-                                        <FormControl>
-                                            <Input className="w-[350px]" placeholder={input.text} {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                        ))
-                    }
-                    <Button className="w-[350px]" type="submit" disabled={treeLaoding}>{treeLaoding ? "Saving..." : "Save"}</Button>
-                </form>
-            </Form>
-        </div>
-    )
-}
+  const form = useForm({
+    defaultValues: {
+      name: card ? card.name : "",
+      scientificName: card?.scientificName || "",
+      treeType: card?.treeType || "",
+      location: card?.location || "",
+      description: card?.description || "",
+      features: card?.features || "",
+      maintenance: card?.maintenance || "",
+      benefits: card?.benefits || "",
+      funFact: card?.funFact || "",
+    },
+  });
+
+  const onSubmit = async (values: any) => {
+    setTreeLoading(true);
+    const treeData = {
+      name: values.name,
+      scientificName: values.scientificName,
+      treeType: values.treeType,
+      location: values.location,
+      description: values.description,
+      features: values.features,
+      maintenance: values.maintenance,
+      benefits: values.benefits,
+      funFact: values.funFact,
+      user: user?._id,
+    };
+    try {
+      if (isTree) {
+        await axios.put(
+          `${import.meta.env.VITE_BASE_URL}/cards/edit/${id}?type=tree`,
+          treeData,
+          { withCredentials: true }
+        );
+        toast.success("Tree VCard Updated");
+      } else {
+        await axios.post(
+          `${import.meta.env.VITE_BASE_URL}/cards/new?type=tree`,
+          treeData,
+          { withCredentials: true }
+        );
+        toast.success("Tree VCard Created");
+      }
+      if (isPaid || user?.role === "admin") {
+        navigate(-1);
+      } else {
+        navigate("/plans");
+      }
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+      if (!isPaid && user?.role !== "admin") {
+        navigate("/plans");
+      }
+    }
+    setTreeLoading(false);
+  };
+
+  return (
+    <>
+        <div className="my-2">
+        <h1 className="font-Kanit font-bold text-3xl pl-8">Tree</h1>
+      </div>
+    <div className="flex flex-col justify-center items-center min-h-screen mb-8 -mt-8">
+      <p>{card?.name!}</p>
+      <div className="flex flex-col justify-center max-h-screen pb-10">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {inputs.map((input, index) => (
+            <div className="flex flex-row gap-6" key={index}>
+              <div className="basis-1/3 flex justify-start items-center">
+                <label
+                  htmlFor=""
+                  className="flex justify-start items-center font-Kanit pl-3"
+                >
+                  {input.label}
+                </label>
+              </div>
+              <div className="basis-2/3 w-[90%] mr-2">
+                <input
+                  type="text"
+                  name={input.name}
+                  id="floating_email"
+                  className="block py-2.5 px-0 w-full text-base font-Kanit bg-transparent border-0 border-b-2 border-black appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 pl-2"
+                  placeholder={input.text}
+                  required
+                />
+              </div>
+            </div>
+          ))}
+          <div className="flex justify-center">
+            <button
+              className="px-4 py-2 rounded-md hover:cursor-pointer w-[90%] bg-red-400 font-Kanit text-lg"
+              type="submit"
+              disabled={treeLoading}
+            >
+              {treeLoading ? "Saving..." : "Save"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    </>
+  );
+};
 
 export default CreateTree;
