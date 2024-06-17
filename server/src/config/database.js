4363d484b5755380444024c78c470e3a670ff77d
RITAP28 <ritap2804manutd@gmail.com>
2024-06-18 01:08:42 +0530

    updated some more frontend things...

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default function connectDatabase() {
     mongoose
-        .connect(process.env.MONGO_URI)
+        .connect("mongodb+srv://umanandasiddha243:7JG0k5f2wITp78oF@cluster0.fheoz2x.mongodb.net/vcards")
         .then((data) => {
             console.log(`Mongodb connected ${data.connection.host}`);
         })
